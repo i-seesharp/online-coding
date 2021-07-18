c2ad52194331e76d4ec740bba5c32887c35e9f34
Aaru Barman <aaranya.barman@mail.utoronto.ca>
2021-07-18 06:38:01 -0400

    backend skeleton

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,36 @@
-const http = require("http");
 const path = require("path");
-const express = require("express");
-const variables = require(path.join(__dirname, "variables.js"));
-const socketio = require("socket.io");
-const server = http.createServer(app);
+const http = require("http");
 const cors = require("cors");
+const express = require("express");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const socketio = require("socket.io");
+const { cookie } = require("./variables");
+const variables = require(path.join(__dirname, "variables.js"));
+const app = express();
+const server = http.createServer(app);
 const sessionStore = session.MemoryStore();
+const PORT = process.env.PORT || variables.port;
 const io = socketio(server, {
-    cors : {
+    cors: {
         credentials: variables.credentials,
         origin: variables.origin
     }
 });
-app.use(cors({credentials : true, origin: variables.origin}));
+
+app.use(cors({ credentials: variables.credentials, origin: variables.origin }));
 app.use(express.json());
 app.use(cookieParser(variables.cookie));
 app.use(session({
-    secret: variables.session,
     cookie: { maxAge: 1000*60*60 },
+    secret: variables.session,
+    store: sessionStore,
     resave: true,
-    saveUninitialized: false,
-    store: sessionStore
+    saveUninitialized: false
 }));
 
 io.on("connection", socket => console.log(socket.id));
 
+server.listen(PORT, () => {
+    console.log("Listening on port : "+PORT);
+});
--- backend/variables.js
+++ backend/variables.js
@@ -2,7 +2,8 @@ const variables = {
     origin: "http://localhost:3000",
     credentials: true,
     cookie: "sOmEsEcReT",
-    session: "sOmEsEcReT"
+    session: "sOmEsEcReT",
+    port: 5000
 }
 
 module.exports = variables;
